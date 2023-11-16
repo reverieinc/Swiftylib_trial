0b81b25c21b0f73366a0b2140a26f268f93578ca
chandanbhagabati <chandan.bhagabati@reverieinc.com>
2023-11-16 15:35:21 +0530

    Initial Commit

---
+++ Swiftylib/Swiftylib.h
@@ -0,0 +1,18 @@
+//
+//  Swiftylib.h
+//  Swiftylib
+//
+//  Created by Chandan Bhagabati on 16/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swiftylib.
+FOUNDATION_EXPORT double SwiftylibVersionNumber;
+
+//! Project version string for Swiftylib.
+FOUNDATION_EXPORT const unsigned char SwiftylibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftylib/PublicHeader.h>
+
+

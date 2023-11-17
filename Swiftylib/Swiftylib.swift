2367dcf7a49e8d546ba7be4c73450c9554716cdc
chandanbhagabati <chandan.bhagabati@reverieinc.com>
2023-11-17 10:51:46 +0530

    Added Test Cases

---
+++ Swiftylib/Swiftylib.swift
@@ -0,0 +1,21 @@
+//
+//  Swiftylib.swift
+//  Swiftylib
+//
+//  Created by Chandan Bhagabati on 16/11/23.
+//
+
+import Foundation
+
+public final class Swiftylib {
+    
+    let name = "Swiftylib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- SwiftylibTests/SwiftylibTests.swift
+++ SwiftylibTests/SwiftylibTests.swift
@@ -10,8 +10,11 @@ import XCTest
 
 final class SwiftylibTests: XCTestCase {
 
+    var swiftylib: Swiftylib!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftylib = Swiftylib()
     }
 
     override func tearDownWithError() throws {
@@ -24,6 +27,14 @@ final class SwiftylibTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(swiftylib.add(a: 1, b: 2), 3)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(swiftylib.sub(a: 3, b: 2), 1)
     }
 
     func testPerformanceExample() throws {
